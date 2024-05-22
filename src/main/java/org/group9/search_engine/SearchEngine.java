8e7934101db25fe50390406e0f795369a35bbb5e
susanomkqn <nammmmk0@gmail.com>
2024-05-22 19:06:27 +0700

    add News object

---
+++ src/main/java/org/group9/search_engine/SearchEngine.java
@@ -0,0 +1,10 @@
+package org.group9.search_engine;
+import java.util.*;
+import com.opencsv.CSVReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class SearchEngine {
+    private List<List<String>> tokenizedCorpus;
+
+}
