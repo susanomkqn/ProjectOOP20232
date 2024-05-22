e64ada7ede7b69f5ed63fc2de452fd2d44c76ea2
susanomkqn <nammmmk0@gmail.com>
2024-05-22 13:31:55 +0700

    add News object

--- src/main/java/org/group9/search_engine/BM25.java
+++ src/main/java/org/group9/search_engine/BM25.java
@@ -1,4 +1,13 @@
 package org.group9.search_engine;
 
 public class BM25 {
+    private static final double k1=1.2;
+    private static final double b=0.75;
+    private static final double delta=1.0;
+    public BM25(){
+        this(1.2,0.75,1.0);
+    }
+
+    public BM25(double k1, double b, double delta) {
+    }
 }
