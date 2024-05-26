07c4f2363b2efd2e8291482aec810210c62ad7d5
susanomkqn <nammmmk0@gmail.com>
2024-05-27 01:49:55 +0700

    add News object

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module ProjectOOP20232 {
+    requires javafx.fxml;
+    requires javafx.controls;
+    requires java.logging;
+    requires com.opencsv;
+    requires org.jsoup;
+    exports org.group9.gui;
+    exports org.group9.news;
+    exports org.group9.scrape;
+    exports org.group9.search_engine;
+}
